55eb1de9a03ca653cd02b05e78999501a1995d8d
o-seonsik <dhtjstlr777@gmail.com>
2021-01-09 14:31:04 +0900

    Modify some files

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const fan = () => {
-    return alert("클릭했습니다.")
+    if(document.getElementById('backend-data')){
+        return alert("이미 정보를 불러왔습니다.")
+    }
+
+    fetch('http://130.211.238.139:5000/')
+        .then(response => response.json())
+        .then(data => {
+            let add_div = document.createElement("div")
+            const dynamicDOM = document.getElementById("get_api")
+
+            const text = "<p>author: " + data.author+ "</p>" + "<p>description: " + data.description+ "</p>" + "<p>date: " + data.date+ "</p>"
+            add_div.innerHTML = text
+            add_div.setAttribute("id", "backend-data")
+            dynamicDOM.appendChild(add_div);
+        })
+        .catch((error) => {
+            console.error('Error:', error)
+        });
 }
